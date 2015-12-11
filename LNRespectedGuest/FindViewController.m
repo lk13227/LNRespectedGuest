c567f98a311dd995d53ab994a16b8c207e2f4303
yun <shiqiaoyun7922@163.com>
2015-12-11 17:40:27 +0800

    设置

--- LNRespectedGuest/FindViewController.m
+++ LNRespectedGuest/FindViewController.m
@@ -13,6 +13,9 @@
 
 @property(nonatomic,strong)UITableView *tableView;
 @property(nonatomic,strong)NSMutableArray *dataArray;
+@property(nonatomic,strong)UILabel *circleLabel;
+@property(nonatomic,strong)UILabel *memberLabel;
+@property(nonatomic,strong)UILabel *peopleLabel;
 
 @end
 
@@ -33,6 +36,13 @@
 //    circleBtn addTarget:<#(nullable id)#> action:<#(nonnull SEL)#> forControlEvents:<#(UIControlEvents)#>
     [self.view addSubview:circleBtn];
     
+    self.circleLabel = [[UILabel alloc]init];
+    //self.circleLabel.backgroundColor = [UIColor lightGrayColor];
+    self.circleLabel.text = @"350";
+    self.circleLabel.textColor = [UIColor greenColor];
+    [self.view addSubview:self.circleLabel];
+    
+    
     //贵元
     UIView *memberView = [[UIView alloc] init];
     memberView.backgroundColor = [UIColor whiteColor];
@@ -43,6 +53,12 @@
     [memberBtn setImage:[UIImage imageNamed:@"发现_05"] forState:UIControlStateNormal];
 //    memberBtn addTarget:<#(nullable id)#> action:<#(nonnull SEL)#> forControlEvents:<#(UIControlEvents)#>
     [self.view addSubview:memberBtn];
+    
+    self.memberLabel = [[UILabel alloc]init];
+    //self.memberLabel.backgroundColor = [UIColor lightGrayColor];
+    self.memberLabel.text = @"350";
+    self.memberLabel.textColor = [UIColor greenColor];
+    [self.view addSubview:self.memberLabel];
     
     //贵人
     UIView *peopleView = [[UIView alloc] init];
@@ -55,49 +71,75 @@
 //    peopleBtn addTarget:<#(nullable id)#> action:<#(nonnull SEL)#> forControlEvents:<#(UIControlEvents)#>
     [self.view addSubview:peopleBtn];
     
+    self.peopleLabel = [[UILabel alloc]init];
+    //self.peopleLabel.backgroundColor = [UIColor lightGrayColor];
+    self.peopleLabel.text = @"350";
+    self.peopleLabel.textColor = [UIColor greenColor];
+    [self.view addSubview:self.peopleLabel];
+    
     //三个背景view的约束
     NSInteger W = self.view.frame.size.width/3;
     [circleView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.mas_equalTo(self.view.mas_left);
         make.width.offset(W);
         make.top.mas_equalTo(self.view.mas_top).offset(+64);
-        make.height.offset(150);
+        make.height.offset(100);
     }];
     [memberView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.mas_equalTo(circleView.mas_right);
         make.width.mas_equalTo(circleView.mas_width);
         make.top.mas_equalTo(self.view.mas_top).offset(+64);
-        make.height.offset(150);
+        make.height.offset(100);
     }];
     [peopleView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.mas_equalTo(memberView.mas_right);
         make.width.mas_equalTo(circleView.mas_width);
         make.top.mas_equalTo(self.view.mas_top).offset(+64);
-        make.height.offset(150);
+        make.height.offset(100);
     }];
     
     //三个按钮的约束
     [circleBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(circleView.mas_left).offset(20);
-        make.right.mas_equalTo(circleView.mas_right).offset(-20);
+        make.left.mas_equalTo(circleView.mas_left).offset(40);
+        make.right.mas_equalTo(circleView.mas_right).offset(-40);
         make.top.mas_equalTo(circleView.mas_top).offset(10);
         make.bottom.mas_equalTo(circleView.mas_bottom).offset(-10);
     }];
     [memberBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(memberView.mas_left).offset(20);
-        make.right.mas_equalTo(memberView.mas_right).offset(-20);
+        make.left.mas_equalTo(memberView.mas_left).offset(40);
+        make.right.mas_equalTo(memberView.mas_right).offset(-40);
         make.top.mas_equalTo(memberView.mas_top).offset(10);
         make.bottom.mas_equalTo(memberView.mas_bottom).offset(-10);
     }];
     [peopleBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(peopleView.mas_left).offset(20);
-        make.right.mas_equalTo(peopleView.mas_right).offset(-20);
+        make.left.mas_equalTo(peopleView.mas_left).offset(40);
+        make.right.mas_equalTo(peopleView.mas_right).offset(-40);
         make.top.mas_equalTo(peopleView.mas_top).offset(10);
         make.bottom.mas_equalTo(peopleView.mas_bottom).offset(-10);
     }];
     
+    //三个label的约束
+    [self.circleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(circleView.mas_top).offset(+5);
+        make.right.mas_equalTo(circleView.mas_right).offset(-5);
+        make.height.offset(25);
+        make.width.offset(35);
+    }];
+    [self.memberLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(memberView.mas_top).offset(+5);
+        make.right.mas_equalTo(memberView.mas_right).offset(-5);
+        make.height.offset(25);
+        make.width.offset(35);
+    }];
+    [self.peopleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(peopleView.mas_top).offset(+5);
+        make.right.mas_equalTo(peopleView.mas_right).offset(-5);
+        make.height.offset(25);
+        make.width.offset(35);
+    }];
+    
     //tableview
-    self.tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 214, self.view.bounds.size.width, self.view.bounds.size.height-214) style:UITableViewStylePlain];
+    self.tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 170, self.view.bounds.size.width, self.view.bounds.size.height-170) style:UITableViewStylePlain];
     self.tableView.scrollEnabled = NO;//设置tableview不滚动
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
---
+++ LNRespectedGuest/SetViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetViewController.h
+//  LNRespectedGuest
+//
+//  Created by 理念空间科技有限公司 on 15/12/11.
+//  Copyright © 2015年 Yun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetViewController : UIViewController
+
+@end
