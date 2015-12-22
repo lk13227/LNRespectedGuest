7dbc327e55c30ac78a93315d11f0dff9c8b6343f
yun <shiqiaoyun7922@163.com>
2015-12-22 15:10:42 +0800

    测试

--- LNRespectedGuest/AppDelegate.m
+++ LNRespectedGuest/AppDelegate.m
@@ -42,7 +42,7 @@
                                                          forBarMetrics:UIBarMetricsDefault];
     /*添加导航控制器*/
     UINavigationController *merchantBusinessNav = [[UINavigationController alloc]initWithRootViewController:self.merchantBusinessVC];
-    merchantBusinessNav.navigationBar.tintColor = [UIColor whiteColor];//改变导航箭头颜色
+    merchantBusinessNav.navigationBar.tintColor = [UIColor whiteColor];//改变导航箭头颜色123456
     [merchantBusinessNav.navigationBar setTitleTextAttributes:
      @{NSFontAttributeName:[UIFont systemFontOfSize:19],
        NSForegroundColorAttributeName:[UIColor whiteColor]}];//改变导航标题颜色
