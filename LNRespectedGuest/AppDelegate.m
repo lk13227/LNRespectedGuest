162abf3b04f8a2f832f1e310a28ad353fce5455e
刘凯 <763149849@qq.com>
2016-01-20 14:19:14 +0800

    支付宝回调增加

--- LNRespectedGuest/AppDelegate.m
+++ LNRespectedGuest/AppDelegate.m
@@ -330,7 +330,7 @@
         NSError *error = nil;
         if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
             // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            // abort() causes the application to generate a crash log and termnate. You should not use this function in a shipping application, although it may be useful during development.
             NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
             abort();
         }
@@ -344,9 +344,17 @@
     if (result == FALSE) {
         //调用其他SDK，例如支付宝SDK等
         
+        //如果极简开发包不可用，会跳转支付宝钱包进行支付，需要将支付宝钱包的支付结果回传给开发包
         if ([url.host isEqualToString:@"safepay"]) {
-            //跳转支付宝钱包进行支付，处理支付结果
             [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+                //【由于在跳转支付宝客户端支付的过程中，商户app在后台很可能被系统kill了，所以pay接口的callback就会失效，请商户对standbyCallback返回的回调结果进行处理,就是在这个方法里面处理跟callback一样的逻辑】
+                NSLog(@"result = %@",resultDic);
+            }];
+        }
+        if ([url.host isEqualToString:@"platformapi"]){//支付宝钱包快登授权返回authCode
+            
+            [[AlipaySDK defaultService] processAuthResult:url standbyCallback:^(NSDictionary *resultDic) {
+                //【由于在跳转支付宝客户端支付的过程中，商户app在后台很可能被系统kill了，所以pay接口的callback就会失效，请商户对standbyCallback返回的回调结果进行处理,就是在这个方法里面处理跟callback一样的逻辑】
                 NSLog(@"result = %@",resultDic);
             }];
         }
